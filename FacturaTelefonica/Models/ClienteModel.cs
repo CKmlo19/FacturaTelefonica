ddf92e0d8a2234b03c56cdd2f36511d83d492e66
Camilo Orocú <luiscamilooc@gmail.com>
2024-04-30 17:47:03 -0600

    Cambiando a base de datos remoto

---
+++ FacturaTelefonica/Models/ClienteModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FacturaTelefonica.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ FacturaTelefonica/Models/ContratoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FacturaTelefonica.Models
+{
+    public class ContratoModel
+    {
+        public int Id { get; set; }
+        public int DocIdentidad { get; set; }
+        public int idTipoTarifa { get; set; }
+        public int NumeroTelefonico { get; set; }
+        public DateTime FechaContrato { get; set; }
+    }
+}
